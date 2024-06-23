85b6506bc13bbb94d0bb0860124f09fbab689b31
Grander78498 <grander.l.l.1242@gmail.com>
2024-06-23 09:11:08 +0300

    Протестированы классы для сущностей из бд

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from src import database
-from src import bot
+# from src import bot
+from src import new_database
 import sys
 import asyncio
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1 and sys.argv[1] == 'drop':
-        database.drop_tables()
-    database.create_tables()
-    asyncio.run(bot.main())
+    if len(sys.argv) > 1:
+        match sys.argv[1]:
+            case 'drop':
+                database.drop_tables()
+            case 'test':
+                new_database.main()
+
+    # database.create_tables()
+    # asyncio.run(bot.main())
--- src/config.py
+++ src/config.py
@@ -6,5 +6,5 @@ load_dotenv()
 host = '127.0.0.1'
 user = 'postgres'
 password = 'postgres'
-dbname = 'test'
+dbname = 'queue_db'
 API_TOKEN = os.getenv("BOT_TOKEN")
