5a8a5739e401cf2d18e0795d0b9fbdb5e7683faf
Grander78498 <grander.l.l.1242@gmail.com>
2024-07-08 08:49:34 +0300

    Добавление моделей django

--- src/backend/django_queue/settings.py
+++ src/backend/django_queue/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'django_queue.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'queue_db',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres'
     }
 }
 
--- src/backend/queue_api/models.py
+++ src/backend/queue_api/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class TelegramUser(models.Model):
+    tg_id = models.BigIntegerField(primary_key=True)
+    full_name = models.CharField(max_length=128)
+    is_admin = models.BooleanField()
+    group = models.ManyToManyField('TelegramGroup')
+    queue = models.ManyToManyField('Queue', through='QueueMember')
+
+
+class TelegramGroup(models.Model):
+    tg_id = models.BigIntegerField(primary_key=True)
+    name = models.CharField(max_length=128)
+    thread_id = models.BigIntegerField(null=True)
+
+
+class Queue(models.Model):
+    message = models.CharField(max_length=64)
+    date = models.DateTimeField()
+    tz = models.IntegerField()
+    creator = models.ForeignKey('TelegramUser', on_delete=models.CASCADE, related_name='creator')
+    group = models.ForeignKey('TelegramGroup', on_delete=models.CASCADE)
+    message_id = models.BigIntegerField(null=True)
+
+
+class QueueMember(models.Model):
+    user = models.ForeignKey('TelegramUser', on_delete=models.CASCADE)
+    queue = models.ForeignKey('Queue', on_delete=models.CASCADE)
+    vote_time = models.DateTimeField(auto_now_add=True)
