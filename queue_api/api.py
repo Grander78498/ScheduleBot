e0283f6ad7fa20ea995f40ece680d68c8839da0d
Xui <adkorolkov@yandex.ru>
2024-07-23 15:14:51 +0300

    Добавлена рабочая команда по смену часового пола

--- queue_api/api.py
+++ queue_api/api.py
@@ -254,7 +254,7 @@ async def change_tz(user_id: int, tz: str):
     if check_timezone(tz):
         tz = int(tz)
     else:
-        return {'status': 'NO', 'message': 'Опять саратовское время вводишь???'}
+        return {'status': 'NO', 'message': 'Опять саратовское время вводишь???. Пробуй ещё'}
 
     await TelegramUser.objects.filter(pk=user_id).aupdate(tz=tz)
     return {'status': 'OK', 'message': 'Справился с вводом одной цифры'}
