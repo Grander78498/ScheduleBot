1914038f7e7250e2fab1ccc02a3ab7ee418e8fe8
Grander78498 <grander.l.l.1242@gmail.com>
2024-07-29 15:03:05 +0300

    Изменения для полноценного запуска бота

--- django_queue/settings.py
+++ django_queue/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 from config import DBNAME, HOST, PORT, USER, PASSWORD
 
@@ -13,9 +14,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tnw%a1u3-b$l5og%+$q4*(62@7l#b9^^$9=8(48b74n71fq&*u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.1.43']
 
 
 # Application definition
@@ -112,6 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
