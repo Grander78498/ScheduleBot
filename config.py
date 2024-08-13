a9622bc2e5c52a7cb7bb1d898895f6bf8f958092
Grander78498 <grander.l.l.1242@gmail.com>
2024-08-13 19:23:40 +0300

    Кхе-кхе (2000)

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ class Settings(BaseSettings):
     bot_token: SecretStr
     api_id: int
     api_hash: SecretStr
-    cypher_key: SecretStr
     host: str = '127.0.0.1'
     port: str = '5432'
     user: str = 'postgres'
