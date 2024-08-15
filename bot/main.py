1302d36c219e328ca50d55bdbbec67604380c63c
Grander78498 <grander.l.l.1242@gmail.com>
2024-08-15 22:34:11 +0300

    Прохладно

--- bot/main.py
+++ bot/main.py
@@ -25,7 +25,7 @@ dp.include_router(echo_router)
 async def main():
     logging.basicConfig(level=logging.INFO)
     logger.info(f"DEBUG = {settings.DEBUG}")
-    await dp.start_polling(bot, allowed_updates=["message", "chat_member"])
+    await dp.start_polling(bot, allowed_updates=["message", "callback_query", "chat_member", "my_chat_member"])
 
 
 if __name__ == "__main__":
